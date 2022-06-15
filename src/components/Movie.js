b529e7c5b54b569f6fc628f7d979413c3dc35b9c
Wolfy <alka-wolfik@yandex.ru>
2022-06-15 03:25:42 +0300

    added rating classes

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,12 +6,23 @@ import cutText from './CutText';
 const { Title, Paragraph, Text } = Typography;
 
 function Movie({ title, date, summary, genres, poster, rating }) {
+    let rateClass;
+
+    if (rating < 3) {
+        rateClass = 'rating rating-up-to-three';
+    } else if (rating > 3 && rating < 5) {
+        rateClass = 'rating rating-up-to-five';
+    } else if (rating > 5 && rating < 7) {
+        rateClass = 'rating rating-up-to-seven';
+    } else if (rating > 7) {
+        rateClass = 'rating rating-up-to-ten';
+    }
 
     return (
         <Card >
             <Image width={183} height={281} src={`https://image.tmdb.org/t/p/w500/${poster}`} />
             <Title level={3}>{title}</Title>
-            <div className='rating'>{rating}</div>
+            <div className={rateClass} >{rating}</div>
             <Text>{date ? format(new Date(date), 'd MMMM, Y') : 'Unknown'}</Text>
             <div className='card-tag'>
                 <Tag>{genres.map(el => {
@@ -20,7 +31,7 @@ function Movie({ title, date, summary, genres, poster, rating }) {
                 <Tag>Drama</Tag>
             </div>
             <Paragraph className='cut-text'>{cutText(summary)}</Paragraph>
-            <Rate count={10}/>
+            <Rate count={10} />
         </Card>
     );
 }
